e1371840f4149f43bc31d1f0f117564286aa4588
Fahim <ashfaq.itsd@goldenharvestbd.com>
2024-08-22 12:48:12 +0600

    text recognition init

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,162 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:gallery_picker/gallery_picker.dart';
+import 'package:gallery_picker/models/media_file.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:photo_view/photo_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? selectedMedia;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Text Recognition of Bill/Invoice",
+        ),
+      ),
+      body: _buildUI(),
+      floatingActionButton: SpeedDial(
+        icon: Icons.add,
+        activeIcon: Icons.close,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.image),
+            label: 'Upload Image',
+            onTap: () async {
+              List<MediaFile>? media = await GalleryPicker.pickMedia(
+                  context: context, singleMedia: true);
+              if (media != null && media.isNotEmpty) {
+                var data = await media.first.getFile();
+                setState(() {
+                  selectedMedia = data;
+                });
+              }
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.camera_alt),
+            label: 'Take Picture',
+            onTap: () async {
+              final ImagePicker _picker = ImagePicker();
+              final XFile? photo =
+                  await _picker.pickImage(source: ImageSource.camera);
+              if (photo != null) {
+                setState(() {
+                  selectedMedia = File(photo.path);
+                });
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUI() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        if (selectedMedia != null)
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      FullScreenImageView(imageFile: selectedMedia!),
+                ),
+              );
+            },
+            child: Center(
+              child: Image.file(
+                selectedMedia!,
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        _extractTextView(),
+      ],
+    );
+  }
+
+  Widget _extractTextView() {
+    if (selectedMedia == null) {
+      return const Center(
+        child: Text("No result."),
+      );
+    }
+    return FutureBuilder<String?>(
+      future: _extractText(selectedMedia!),
+      builder: (context, snapshot) {
+        return Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: SingleChildScrollView(
+              child: Text(
+                snapshot.data ?? "",
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<String?> _extractText(File file) async {
+    final textRecognizer = TextRecognizer(
+      script: TextRecognitionScript.latin,
+    );
+    final InputImage inputImage = InputImage.fromFile(file);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+    String text = recognizedText.text;
+    textRecognizer.close();
+    return text;
+  }
+}
+
+class FullScreenImageView extends StatelessWidget {
+  final File imageFile;
+
+  const FullScreenImageView({Key? key, required this.imageFile})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: PhotoView(
+          imageProvider: FileImage(imageFile),
+          backgroundDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
